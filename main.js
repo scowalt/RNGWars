f50b8c549ca63979cafd7c1cc35673e008a55995
Scott Walters <srwalte2@illinois.edu>
2013-12-22 16:21:35 -0600

    Added a bunch of needed packages

--- main.js
+++ main.js
@@ -1,29 +1,35 @@
 // LIBRARY IMPORTS
 var express = require('express');
-var util = require('util');
 var force = require('express-force-domain');
+var IO = require('socket.io');
+var RedisStore = require('connect-redis')(express);
+var SessionSockets = require('session.socket.io');
+var util = require('util');
 
 // MODULE IMPORTS
 var prefs = require(__dirname + '/prefs/prefs.js');
 var secrets = require(__dirname + '/prefs/secrets');
 var routes = require(__dirname + '/routes');
 
+// SETUP
 var app = express();
+var io = IO.listen(app.listen(prefs.port));
+var sessionStore = new RedisStore(secrets.redis);
+var cookieParser = express.cookieParser(secrets.secret);
+var sessionSockets = new SessionSockets(io, sessionStore, cookieParser);
 
 // configure Express
 app.configure(function() {
   app.set('views', __dirname + '/views');
   app.set('view engine', 'ejs');
   if (prefs.logging.express) app.use(express.logger());
-  app.use(express.cookieParser());
+  app.use(cookieParser);
   app.use(express.bodyParser());
   app.use(express.methodOverride());
   app.use(express.static(__dirname + '/public'));
-  app.use(express.session({ secret: 'keyboard cat' }));
+  app.use(express.session({ secret: secrets.secret }));
   app.use(force(prefs.serverUrl));
   app.use(app.router);
 });
-
-app.listen(prefs.port);
 
 app.get('/', routes.index);

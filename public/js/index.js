6c4d67b2ef2e19861ebfa40dc900d251f6441905
Scott Walters <srwalte2@illinois.edu>
2013-12-22 19:23:54 -0600

    Starting to validate inputs

--- public/js/index.js
+++ public/js/index.js
@@ -1,15 +1,21 @@
-$(document).ready(function(){
+$(document).ready(function() {
 	io = io.connect();
 
-	io.on('connect', function onConnect(){
+	io.on('connect', function onConnect() {
 		io.emit('ready', null);
 	});
-});
 
-$('#register_form').submit(function onSubmit(event){
-	event.preventDefault();
-	var username = $('#register_username').val();
-	var password1 = $('#register_password').val();
-	var password2 = $('#register_password2').val();
-	var captcha = $('#captcha').val();
-})
+	$('#register_form').submit(function onSubmit(event) {
+		event.preventDefault();
+		var username = $('#register_username').val();
+		if (!validateUsername(username)) return;
+		var password1 = $('#register_password').val();
+		var password2 = $('#register_password2').val();
+		var captcha = $('#captcha').val();
+	});
+
+	function validateUsername(username) {
+		if (typeof username !== 'string')
+			return;
+	};
+});

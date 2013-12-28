54a54e16c4e66b9b905d9ed41982d50137eeeb6f
Scott Walters <srwalte2@illinois.edu>
2013-12-28 14:48:10 -0600

    Redirects to game if already logged in

--- routes/index.js
+++ routes/index.js
@@ -3,6 +3,9 @@ var captcha = require('../utils/captcha.js');
 module.exports = function onRequire(passport) {
 	return {
 		index: function(req, res) {
+			if (req.isAuthenticated()){
+				res.redirect('/game');
+			}
 			res.render('index', {
 				captcha: captcha.createCaptcha()
 			});
